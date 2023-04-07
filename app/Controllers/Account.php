c04555345b08b2c2088b7d7c0e83b7186eaf4773
dahni <ekodahni@gmail.com>
2023-04-07 19:29:37 +0000

    update function update password

--- app/Controllers/Account.php
+++ app/Controllers/Account.php
@@ -8,10 +8,29 @@ class Account extends BaseController
         return view('Dashboard/Account/changePassword');
     }
     public function updatePassword(){
+        $isValid = [
+            'currentPw' => 'required',
+            'password' => 'required|min_length[8]',
+            'confirmPw' => [
+                'label' => 'Confirm Password',
+                'rules' => 'required|matches[password]',
+                'errors' => [
+                    'required' => 'The Confirmation Password field is required.',
+                    'matches' => 'The Confirmation Password field does not match the Password field.',
+                ]
+            ]
+        ];
+        if (!$this->validate($isValid)) {
+            $html = $this->isvalid->listErrors();
+            $oneline = preg_replace('/\s+/', ' ', $html);
+            $this->sesi->setFlashdata('validation', $oneline);
+            return redirect()->to('dashboard/changePassword');
+        }
         $userid = session()->get('userid');
         $enp = 'api/updatePassword';
         $dataBody = [
             'userid' => $userid,
+            'currentPw' => $this->request->getVar('currentPw'),
             'password' => $this->request->getVar('password')
         ];
         $postData = $this->async->post($enp, $this->apiclient, $dataBody);
@@ -20,7 +39,7 @@ class Account extends BaseController
             $this->sesi->setFlashdata('sukses', "Congratulations, you have successfully update password");
             return redirect()->to('dashboard/changePassword');
         }else{
-            $this->sesi->setFlashdata('error', "Sorry, check again your data");
+            $this->sesi->setFlashdata('error', $parseData);
             return redirect()->to('dashboard/changePassword');
         }
     }
