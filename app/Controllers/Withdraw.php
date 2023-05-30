c3728e47a76382b94af8c66ca27f3cb11d9d0c9d
dahni <ekodahni@gmail.com>
2023-05-30 00:41:39 +0000

    test

--- app/Controllers/Withdraw.php
+++ app/Controllers/Withdraw.php
@@ -13,6 +13,9 @@ class Withdraw extends BaseController
             ];
             $postData = $this->async->post($enp, $this->apimain, $dataBody);
             $parseData = $postData->response;
+            if (is_object($parseData->dataTrans) && !is_countable($parseData->dataTrans)) {
+                $parseData->dataTrans = [$parseData->dataTrans];
+            }
             $data = [
                 "dataTrans" => $parseData->dataTrans,
             ];
