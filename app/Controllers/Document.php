77b4da286926ddd657798db4d6c9d04ff5969342
MasDahni1337 <ekodahni@gmail.com>
2023-06-06 23:04:07 +0700

    update documentation api

--- app/Controllers/Document.php
+++ app/Controllers/Document.php
@@ -6,6 +6,23 @@ class Document extends BaseController
 {
     public function index()
     {
-        return view('Dashboard/Documentation/index');
+        if ($this->sesi->get('role') == 1) {
+            return view('Dashboard/Main/Documentation/index');
+        } elseif($this->sesi->get('role') == 2) {
+            $enp = 'api/account/personalKey';
+            $dataBody = [
+                'userID' => $this->sesi->get('userid')
+            ];
+            $postData = $this->async->post($enp, $this->apiclient, $dataBody);
+            $parseData = $postData->response;
+            if (is_countable($parseData)) {
+                $parseData = $parseData[0];
+            }
+            $data = [
+                "dataKey" => $parseData,
+            ];
+            return view('Dashboard/Client/Documentation/index', $data);
+        }
+        
     }
 }
