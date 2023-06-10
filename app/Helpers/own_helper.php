0545fa44d7cfeca433d8afae287d6be4adb14c06
MasDahni1337 <ekodahni@gmail.com>
2023-06-10 11:18:51 +0700

    update format currency

--- app/Helpers/own_helper.php
+++ app/Helpers/own_helper.php
@@ -3,7 +3,7 @@ if (!function_exists('formatKRW')) {
     function formatKRW($amount)
     {
         $formatter = new \NumberFormatter('ko_KR', \NumberFormatter::CURRENCY);
-        $formatter->setAttribute(\NumberFormatter::MAX_FRACTION_DIGITS, 3);
+        $formatter->setAttribute(\NumberFormatter::MAX_FRACTION_DIGITS, 2);
         $formatter->setAttribute(\NumberFormatter::MIN_FRACTION_DIGITS, 2); // Add this line
 
         if($amount == '' || $amount == null){
