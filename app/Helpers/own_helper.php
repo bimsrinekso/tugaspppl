99ca4e57d452636f1dc3d8d7cfb3671ca02f75d7
root <root@SyahiidBaniPutra.localdomain>
2023-06-11 18:26:35 +0700

    change own_helper

--- app/Helpers/own_helper.php
+++ app/Helpers/own_helper.php
@@ -16,25 +16,6 @@ if (!function_exists('formatKRW')) {
 }
 
 
-// if (!function_exists('format_date')) {
-//     /**
-//      * Format a date using DateTime
-//      *
-//      * @param string $date The date in the format 'Y-m-d\TH:i:s.u\Z'
-//      * @param string $format The desired format, e.g., 'd-m-Y H:i:s'
-//      * @param string $timezone The desired timezone, default is 'UTC'
-//      * @return string The formatted date
-//      */
-//     function format_date(string $date, string $format, string $timezone = 'UTC'): string
-//     {
-//         $dateTime = DateTime::createFromFormat('Y-m-d\TH:i:s.u\Z', $date, new DateTimeZone('UTC'));
-//         $dateTime->setTimezone(new DateTimeZone($timezone));
-//         return $dateTime->format($format);
-//     }
-// }
-
-
-
 if (!function_exists('format_date')) {
     /**
      * Format a date using DateTime
@@ -56,13 +37,6 @@ if (!function_exists('format_date')) {
         $targetTimezone = new DateTimeZone($timezone);
         $dateTime->setTimezone($targetTimezone);
         
-        // Check if DST is in effect for the target timezone
-        if ($targetTimezone->getTransitions($dateTime->getTimestamp(), $dateTime->getTimestamp())) {
-            // DST is in effect, subtract 1 hour
-            $dateTime->sub(new DateInterval('PT1H'));
-        }
-        
         return $dateTime->format($format);
     }
 }
-
