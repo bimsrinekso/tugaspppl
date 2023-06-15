0feb59db1e55c5470970bf2440723978cec29290
MasDahni1337 <ekodahni@gmail.com>
2023-06-16 00:48:37 +0700

    update dashboard

--- app/Views/Dashboard/Main/index.php
+++ app/Views/Dashboard/Main/index.php
@@ -37,26 +37,6 @@
                             <div class="card-body">
                                 <div class="d-flex">
                                     <div class="flex-grow-1">
-                                        <p class="text-muted fw-medium">Total Users</p>
-                                        <h4 class="mb-0"><?=$totalUser?></h4>
-                                    </div>
-
-                                    <div class="flex-shrink-0 align-self-center">
-                                        <div class="mini-stat-icon avatar-sm rounded-circle bg-primary">
-                                            <span class="avatar-title">
-                                                    <i class="bx bx-group font-size-24"></i>
-                                                </span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="card mini-stats-wid">
-                            <div class="card-body">
-                                <div class="d-flex">
-                                    <div class="flex-grow-1">
                                         <p class="text-muted fw-medium">Total Balance Main</p>
                                         <h4 class="mb-0"><?= formatKRW($totalBlcMain)?></h4>
                                     </div>
