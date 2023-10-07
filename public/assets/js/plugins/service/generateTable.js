9a8a06b2577c424b465bc57c08ebf83315b49932
MasDahni1337 <ekodahni@gmail.com>
2023-10-07 21:33:00 +0700

    add client-side list depo

--- public/assets/js/plugins/service/generateTable.js
+++ public/assets/js/plugins/service/generateTable.js
@@ -28,16 +28,18 @@ generateTable = (idTable, url, columns, order, startDate, endDate) => {
         );
       },
       success: function (data) {
-        console.log(data);
         $(idTable + " tbody").empty();
         table.clear().draw();
-        table.rows.add(data.data).draw();
-        table.column(0, {
-          search: 'applied',
-          order: 'applied'
-        }).nodes().each(function (cell, i) {
-          cell.innerHTML = i + 1;
-        });
+        if(data.data != null){
+          table.rows.add(data.data).draw();
+          table.column(0, {
+            search: 'applied',
+            order: 'applied'
+          }).nodes().each(function (cell, i) {
+            cell.innerHTML = i + 1;
+          });
+        }
+        
       },
       error: function (xhr, error, thrown) {
         console.log('XHR:', xhr);
